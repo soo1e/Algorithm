086fac94926970426c03d444bde5e7c987b87f48
soo <121608565+soo1e@users.noreply.github.com>
2024-01-26 14:09:30 +0900

    [level 0] Title: 문자열 돌리기, Time: 1.66 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    for (int i = 0; i < str.length(); ++i) {
+        cout << str[i] << endl;
+    }
+}
