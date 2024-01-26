4ef3ecfdbb2dda34bd9b74d723f37e532a2b33b8
soo <121608565+soo1e@users.noreply.github.com>
2024-01-26 14:12:13 +0900

    [level 0] Title: 홀짝 구분하기, Time: 1.56 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if ( n % 2 == 0 ) {
+        cout << n << " is even" << endl;
+    } else {
+        cout << n << " is odd" << endl;
+    }
+}
