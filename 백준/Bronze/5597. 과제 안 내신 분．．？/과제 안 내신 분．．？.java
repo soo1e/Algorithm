e4c42c978a888e35b9e5cbd40613d2e919ac551f
soo <121608565+soo1e@users.noreply.github.com>
2024-02-04 15:23:32 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> students = new ArrayList<Integer>();
+        for (int i = 1; i <= 30; i++) {
+            students.add(i);
+        }
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 28; i++) {
+            int N = sc.nextInt();
+            students.remove(Integer.valueOf(N));
+        }
+
+        System.out.println(students.get(0));
+        System.out.println(students.get(1));
+
+    }
+}
