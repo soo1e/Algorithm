edf4c4c321cd0e87d763b3f8ec3cf4e3ecf21e31
soo <121608565+soo1e@users.noreply.github.com>
2024-02-06 23:19:17 +0900

    [Bronze IV] Title: R2, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int R1 = scanner.nextInt();
+        int S = scanner.nextInt();
+        int R2 = (2*S) - R1;
+        
+        System.out.println(R2);
+    }
+}
