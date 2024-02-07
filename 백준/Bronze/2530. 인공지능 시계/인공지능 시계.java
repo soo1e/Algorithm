27af64dfeacf1e318b18789ca93ba0cc2b52a820
soo <121608565+soo1e@users.noreply.github.com>
2024-02-07 21:00:19 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 228 ms, Memory: 18352 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+
+        int D = scanner.nextInt();
+
+        C = C + D;
+
+        B = B + (C/60);
+        C = C % 60;
+
+        A = A + (B/60);
+        B = B%60;
+        A = A%24;
+
+        System.out.print(A + " ");
+        System.out.print(B + " ");
+        System.out.print(C);
+
+    }
+}
