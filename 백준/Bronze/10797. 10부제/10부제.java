960aacaef9322e9d680881c313bd1dbac27c885c
soo <121608565+soo1e@users.noreply.github.com>
2024-02-07 21:06:29 +0900

    [Bronze IV] Title: 10부제, Time: 220 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int[] arr = new int[5];
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int A = scanner.nextInt();
+            arr[i] = A;
+            if (arr[i] == N) {
+                sum = sum + 1;
+            }
+        }
+        System.out.println(sum);
+    }
+}
