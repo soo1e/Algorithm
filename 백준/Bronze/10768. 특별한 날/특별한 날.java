c971aeef12518504a54897d568fe3164f3657b12
soo <121608565+soo1e@users.noreply.github.com>
2024-02-08 17:45:42 +0900

    [Bronze IV] Title: 특별한 날, Time: 204 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int month = scanner.nextInt();
+        int day = scanner.nextInt();
+
+        if (month == 2 && day == 18) {
+            System.out.println("Special");
+        } else if (month < 2) {
+            System.out.println("Before");
+        } else if (month == 2 && day < 18) {
+            System.out.println("Before");
+        } else {
+            System.out.println("After");
+        }
+    }
+}
