b01d9328183a4f8feb472ebed7d3865b32d63b01
soo <121608565+soo1e@users.noreply.github.com>
2024-02-12 18:07:35 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 212 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int S = scanner.nextInt();
+        int K = scanner.nextInt();
+        int H = scanner.nextInt();
+
+        if (S + K + H >= 100) {
+            System.out.println("OK");
+        } else {
+            int min = Math.min(S, Math.min(K, H));
+            if (S == min) {
+                System.out.println("Soongsil");
+            }
+            if (K == min) {
+                System.out.println("Korea");
+            }
+            if (H == min) {
+                System.out.println("Hanyang");
+            }
+        }
+    }
+}
