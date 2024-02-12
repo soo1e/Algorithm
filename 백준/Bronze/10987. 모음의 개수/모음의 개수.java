5d23d56f912162e54f15f47159d9062c019ec9b4
soo <121608565+soo1e@users.noreply.github.com>
2024-02-12 16:56:06 +0900

    [Bronze IV] Title: 모음의 개수, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/10987. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0 ;
         String str = scanner.nextLine();
+        char[] textArr = str.toCharArray();
 
-        int sum = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i); // 문자열의 각 문자에 접근
-            // 문자가 모음인지 확인
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                sum += 1;
+        for (int i = 0; i < textArr.length; i++) {
+            if (textArr[i] == 'a' || textArr[i] == 'e' || textArr[i] == 'i' || textArr[i] == 'o' || textArr[i] == 'u') {
+                sum = sum + 1;
             }
         }
-
         System.out.println(sum);
     }
 }
