92799dbbff59dd2feee19261862ae137f798581b
soo <121608565+soo1e@users.noreply.github.com>
2024-02-12 17:24:53 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 208 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        if (M>0) {
+            for (int i = 0; i < N; i++) {
+                char[] text = scanner.next().toCharArray();
+                for (int j = M-1; j>=0; j--) {
+                    System.out.print(text[j]);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
