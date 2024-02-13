2e03ea7ec9246c2425f8609d50787ae144492831
soo <121608565+soo1e@users.noreply.github.com>
2024-02-13 14:10:01 +0900

    [Bronze III] Title: 최소, 최대, Time: 1744 ms, Memory: 318392 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int[] arr = new int[N];
+
+        int min = 1000000;
+        int max = -1000000;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println(min);
+        System.out.println(max);
+    }
+}
