003f088b8909ddcdc430b1837693f8ff5d508d45
soo <121608565+soo1e@users.noreply.github.com>
2024-02-13 18:00:11 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = new int[9];
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+        for (int i = 0; i < 9; i++) {
+            array[i] = scanner.nextInt();
+            if (max < array[i]) {
+                max = array[i];
+                index = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
