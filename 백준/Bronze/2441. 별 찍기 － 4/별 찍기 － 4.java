339aa2d889908a64d219f08d9ef0cce2355cf90c
soo <121608565+soo1e@users.noreply.github.com>
2024-02-14 17:14:51 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 368 ms, Memory: 18580 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j > 0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = N-i; j > 0 ; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
