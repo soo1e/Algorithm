2f25594e0efff75c8cdfffd3a2b86fb7e4cb4506
soo <121608565+soo1e@users.noreply.github.com>
2024-03-04 18:03:56 +0900

    [Bronze III] Title: 택시 기하학, Time: 208 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        double pi = Math.PI;
+
+        double A = N * N * pi;
+        double B = (N - 0) * (N - 0) + (N -0) * (N - 0);
+
+        System.out.printf("%.6f",A);
+        System.out.println();
+        System.out.printf("%.6f",B);
+    }
+}
