90b24ea12aed35e9194da0f6037a52ff381c0f4c
soo <121608565+soo1e@users.noreply.github.com>
2024-03-06 17:09:15 +0900

    [Bronze III] Title: 네 수, Time: 208 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+        long D = scanner.nextLong();
+
+        String str1 = String.valueOf(A) + String.valueOf(B);
+        String str2 = String.valueOf(C) + String.valueOf(D);
+
+        long num1 = Long.parseLong(str1);
+        long num2 = Long.parseLong(str2);
+        
+        System.out.println(num1 + num2);
+
+    }
+}
