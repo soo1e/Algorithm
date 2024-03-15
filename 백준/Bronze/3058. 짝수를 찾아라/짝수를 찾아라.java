c5aca63f106d8d25cfdcdf4c176d4a47c8a5852d
soo <121608565+soo1e@users.noreply.github.com>
2024-03-15 17:42:05 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 520 ms, Memory: 30176 KB -BaekjoonHub

---
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+            ArrayList<Integer> arrList = new ArrayList<Integer>();
+            int sum = 0;
+            for (int j = 0; j < 7; j++) {
+                int A = scanner.nextInt();
+                if (A % 2 == 0) {
+                    arrList.add(A);
+                }
+                arrList.sort(Comparator.naturalOrder());
+                sum = arrList.stream().mapToInt(Integer::intValue).sum();
+            }
+            System.out.println(sum + " " + arrList.get(0));
+        }
+    }
+}
