949eaf08913751c7af54857bf57fef5a481845da
soo <121608565+soo1e@users.noreply.github.com>
2024-03-15 17:25:47 +0900

    [Bronze III] Title: 수도요금, Time: 212 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/10707. 수도요금/수도요금.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        int D = scanner.nextInt();
+        int P = scanner.nextInt();
+
+        int X = A * P;
+        int Y = 0;
+        if (P >= C) {
+            Y = B + (D * (P-C));
+        } else {
+            Y = B;
+        }
+
+        if (X > Y) {
+            System.out.println(Y);
+        } else {
+            System.out.println(X);
+        }
+
+    }
+}
