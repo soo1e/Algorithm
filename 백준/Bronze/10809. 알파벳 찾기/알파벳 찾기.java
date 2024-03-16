a10331be233b741f05d0b4ab60c5bf7b5a0ad121
soo <121608565+soo1e@users.noreply.github.com>
2024-03-16 22:55:06 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 228 ms, Memory: 18316 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int[] arr = new int[26];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                int index = ch - 'a';
+                if (arr[index] == -1) {
+                    arr[index] = i;
+                }
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
