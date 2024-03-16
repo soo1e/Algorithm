f1509f770b6332953bd4f518c60d5e9cc8223ffc
soo <121608565+soo1e@users.noreply.github.com>
2024-03-16 17:16:03 +0900

    [Bronze III] Title: 앵그리 창영, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int W = scanner.nextInt();
+        int H = scanner.nextInt();
+        int D = (int)Math.sqrt(W * W + H * H);
+
+        for (int i = 0; i < N; i++) {
+            int A = scanner.nextInt();
+            if (A <= D) {
+                System.out.println("DA");
+            } else {
+                System.out.println("NE");
+            }
+        }
+    }
+}
