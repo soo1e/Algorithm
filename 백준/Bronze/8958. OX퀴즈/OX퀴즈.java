f2ea7cfd6ba7450a33c18d71c4f332438c989eda
soo <121608565+soo1e@users.noreply.github.com>
2024-03-17 16:54:05 +0900

    [Bronze II] Title: OX퀴즈, Time: 240 ms, Memory: 18572 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < T; i++) {
+            String str = scanner.nextLine();
+            int count = 0;
+            int sum = 0;
+            for (int j = 0; j < str.length(); j++) {
+                char[] charArr = str.toCharArray();
+                if (charArr[j] == 'O') {
+                    count++;
+                    sum = sum + count;
+                } else {
+                    count = 0;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
