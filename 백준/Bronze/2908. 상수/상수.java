6093c5b095d25d6fef1631e65f068932df980541
soo <121608565+soo1e@users.noreply.github.com>
2024-03-17 17:03:30 +0900

    [Bronze II] Title: 상수, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int revA = (((A%100) % 10) * 100) + (((A/10) % 10) * 10) + (A / 100);
+        int revB = (((B%100) % 10) * 100) + (((B/10) % 10) * 10) + (B / 100);
+
+        if (revA > revB) {
+            System.out.println(revA);
+        } else {
+            System.out.println(revB);
+        }
+    }
+}
