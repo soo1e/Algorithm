ab53b290f2c0f54b38e89ef4f9c8fe4e3a42fd70
soo <121608565+soo1e@users.noreply.github.com>
2024-03-18 17:56:50 +0900

    [Bronze II] Title: 벌집, Time: 208 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int count = 1; // 거리
+        int range = 2; // 범위
+
+        if (N == 1) {
+            System.out.println(1);
+        } else {
+            while (range <= N) {
+                range = range + (6 * count);
+                count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
