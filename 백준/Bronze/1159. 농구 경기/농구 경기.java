064f84ed6ae38823598f38c8ffa5ea446f631e3a
soo <121608565+soo1e@users.noreply.github.com>
2024-03-22 17:13:41 +0900

    [Bronze II] Title: 농구 경기, Time: 220 ms, Memory: 17964 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.nextLine();
+        int[] arr = new int[26];
+        for (int i = 0; i < N; i++) {
+            String str = scanner.nextLine();
+            char[] charArr = str.toCharArray();
+            arr[charArr[0]-'a']++;
+        }
+
+        boolean printed = false;
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] >= 5) {
+                System.out.print((char)(i+'a'));
+                printed = true;
+            }
+        }
+        if(!printed) System.out.println("PREDAJA");
+    }
+}
