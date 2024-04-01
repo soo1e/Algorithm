83cf1167baa54d18fb636241549cc69c633f1111
soo <121608565+soo1e@users.noreply.github.com>
2024-04-01 21:29:54 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,12 +1,8 @@
 class Solution {
     public int[] solution(long n) {
-        // 입력된 숫자의 자릿수 계산
         int digitCount = (int)(Math.log10(n) + 1);
-        
-        // 자릿수만큼의 공간을 가지는 배열 선언
         int[] answer = new int[digitCount];
         
-        // 숫자를 뒤집어서 answer 배열에 저장
         for (int i = 0; i < digitCount; i++) {
             answer[i] = (int)(n % 10);
             n /= 10;
