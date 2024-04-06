1f16d9bb0ca366ff9d6f8ea58d5577604b702d18
soo <121608565+soo1e@users.noreply.github.com>
2024-04-06 17:12:58 +0900

    [level 0] Title: 가위 바위 보, Time: 2.09 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        for (int i = 0; i < rsp.length(); i++) {
+            char current = rsp.charAt(i);
+            if (current == '2') { 
+                answer += '0';
+            } else if (current == '0') { 
+                answer += '5';
+            } else if (current == '5') { 
+                answer += '2';
+            }
+        }
+        return answer;
+    }
+}
