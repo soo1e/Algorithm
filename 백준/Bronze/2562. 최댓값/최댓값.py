e7851db52fb90c3b9765cd8f5ec6006e31009b86
soo <121608565+soo1e@users.noreply.github.com>
2025-02-11 12:17:42 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,12 +1,6 @@
-new_list = []
-max_int = 0
-max_idx = None
-for i in range(0, 9):
-    A = int(input())
-    new_list.append(A)
-    if A > max_int:
-        max_int = A
-        max_idx = i
+numbers = [int(input()) for _ in range(9)]  # 9개의 숫자를 입력받아 리스트로 저장
+max_value = max(numbers)  # 최댓값 찾기
+max_index = numbers.index(max_value) + 1  # 최댓값의 위치 (1부터 시작하는 인덱스)
 
-print(max_int)
-print(max_idx + 1)
+print(max_value)
+print(max_index)
