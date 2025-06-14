4c10f2a22e409f269aba060ad8515326cb456b0d
soo <121608565+soo1e@users.noreply.github.com>
2025-06-14 23:48:29 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.40 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,15 +1,16 @@
 def calculate_expiry_date(start_date, months):
-    
     year, month, day = map(int, start_date.split('.'))
     month += months
     year += (month - 1) // 12
     month = (month - 1) % 12 + 1
     return f"{year:04}.{month:02}.{day:02}"
 
+
 def solution(today, terms, privacies):
     term_dict = {term.split()[0]: int(term.split()[1]) for term in terms}
     expired_privacies = []
 
+    
     for idx, privacy in enumerate(privacies):
         date, term_type = privacy.split()
         expiry_date = calculate_expiry_date(date, term_dict[term_type])
@@ -17,4 +18,5 @@ def solution(today, terms, privacies):
         if expiry_date <= today:
             expired_privacies.append(idx + 1)
 
+            
     return expired_privacies
